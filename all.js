3f2263ab8c5a3d937ff32bfb9a9e9aa6ca85fd38
Mirco <mirco.cotugno@gmail.com>
2020-06-12 17:39:43 +0200

    Revert "Header footer yevgeniya kulikova"

--- all.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu(id) {
-  console.log(id);
-
-  var menu = document.getElementById(id);
-  document.getElementById('buttonMenu').classList.toggle('closed');
-  document.getElementById('voiceMenu').classList.toggle('invisible');
-  menu.classList.toggle('visible');
-
-}
